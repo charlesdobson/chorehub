de97ae9d32231d23a797de533c8e3cad5d681cf4
charlesdobson <charlesdobson92@gmail.com>
2021-05-24 22:48:05 -0400

    feat(routing): style navbar for mobile

--- client/src/components/Navigation/BottomNav.tsx
+++ client/src/components/Navigation/BottomNav.tsx
@@ -1,14 +1,43 @@
 import { createElement, ReactElement } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { ROUTES } from 'constants/routes';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-interface INavButtonProps {
+const Footer = styled.footer`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 6vh;
+`;
+
+const Nav = styled.nav`
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+`;
+
+interface INavLinkProps {
   isActive: boolean;
 }
-const NavButton = styled.button<INavButtonProps>`
-  all: unset;
-  background: ${(props) => (props.isActive ? 'blue' : 'red')};
+const NavLink = styled.a<INavLinkProps>`
+  text-decoration: none;
+  cursor: pointer;
+  background: white;
+  border-top: 1px solid #aaaaaa;
+
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      margin-top: -1px;
+      border-top: 2px solid #111111;
+    `}
 `;
 
 const BottomNav = (): ReactElement => {
@@ -18,18 +47,20 @@ const BottomNav = (): ReactElement => {
   const handleNavClick = (path: string) => history.push(path);
 
   return (
-    <nav>
-      {Object.entries(ROUTES).map(([, route]) => (
-        <NavButton
-          key={route.path}
-          isActive={route.path == pathname}
-          onClick={() => handleNavClick(route.path)}
-        >
-          {createElement(route.icon)}
-          {route.title}
-        </NavButton>
-      ))}
-    </nav>
+    <Footer>
+      <Nav>
+        {Object.entries(ROUTES).map(([, route]) => (
+          <NavLink
+            key={route.path}
+            isActive={route.path == pathname}
+            onClick={() => handleNavClick(route.path)}
+          >
+            {createElement(route.icon)}
+            {route.title}
+          </NavLink>
+        ))}
+      </Nav>
+    </Footer>
   );
 };
 
