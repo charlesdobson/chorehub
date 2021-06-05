2264c92b4350e80f134d99b21ac77660d5bb1242
charlesdobson <charlesdobson92@gmail.com>
2021-06-05 08:12:49 -0400

    feat(styling): add styles for user menu

--- client/src/components/Navigation/shared/UserMenu.tsx
+++ client/src/components/Navigation/shared/UserMenu.tsx
@@ -1,7 +1,31 @@
 import { ReactElement } from 'react';
+import styled from 'styled-components';
+import { mixin } from 'styles';
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 3.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: blue;
+`;
+
+const DropdownButton = styled.button`
+  all: unset;
+  ${mixin.clickable};
+  height: 80%;
+  width: 80%;
+  border-radius: 50%;
+  background: red;
+`;
 
 const UserMenu = (): ReactElement => {
-  return <div>UserMenu</div>;
+  return (
+    <Wrapper>
+      <DropdownButton>UserMenu</DropdownButton>
+    </Wrapper>
+  );
 };
 
 export default UserMenu;
